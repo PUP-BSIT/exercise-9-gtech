d817032c946bf7ae0c1665a03f9c05bc2e37036f
Lopez_HosheaShania <hosheashaniaclopez@iskolarngbayan.pup.edu.ph>
2025-04-08 12:13:34 +0800

    add main file with to do tasks

---
+++ main.py
@@ -0,0 +1,47 @@
+def add_student():
+    # TODO (Grace Lim):
+    # Add a function that allows the user to input the following student details:
+    # - Student ID (Format: 2023-XXXX-XX-X)
+    # - Full Name
+    # - Program
+    pass
+
+
+def list_students():
+    # TODO (Rain Romero):
+    # Add a function to list/display all student records in the format:
+    # Student ID : 2023-XXXX-XX-X, Student Name : Juan Tamad, Student Program : BSIT
+    pass
+
+
+def update_student():
+    # TODO (Althea Aragon):
+    # Add a function to update an existing student record.
+    # - First, show the list of student records
+    # - Allow the user to input the index
+    # - Let the user update name and/or program
+    pass
+
+
+def delete_student():
+    # TODO (Hoshea Lopez):
+    # Add a function to delete a student record.
+    # - Show the list of student records
+    # - Allow the user to input the index
+    # - Confirm before deletion
+    pass
+
+
+def search_student():
+    # TODO (Adriel Dimayuga):
+    # Add a function to search for a student record.
+    # - Allow searching by ID, name, or program
+    # - Display matching records
+    pass
+
+
+def exit_program():
+    # TODO (Grace Lim):
+    # Add a function to exit the program.
+    # - Optionally confirm exit before quitting
+    pass
