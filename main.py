6b9ad81ad93c67aeef39fb7903ea8149147c35cf
Adriel Joseph Dimayuga <dimayuga.adrieljoseph03@gmail.com>
2025-04-08 13:53:48 +0800

    add to do tasks

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
+# Make the code readable as it is
+# add comments per block or if necessary
+# make sure to avoid magic values
+# add tasks and assign it to a member if there is anything else needed
+
 def add_student():
     # TODO (Grace Lim):
-    # Add a function that allows the user to input the following student details:
-    # - Student ID (Format: 2023-XXXX-XX-X)
+    # Add a function that allows the user to input the
+    # following student details:
+    # A data record should contain 5 fields
+    # - Student ID (Max 9 numbers) (Primary Key)
     # - Full Name
     # - Program
+    # - Contact Number (Max 11 numbers)
+    # - Address
+    # All records should be contained in a list
+    # Add restrictions
+    # Ask if the user wants to add another student record (y / n)
+    # Add clear screen everytime the user adds another student record
     pass
 
 
 def list_students():
     # TODO (Rain Romero):
     # Add a function to list/display all student records in the format:
-    # Student ID : 2023-XXXX-XX-X, Student Name : Juan Tamad, Student Program : BSIT
+    # Student ID :               123456789
+    # Student Name :             Juan Tamad
+    # Student Program :          BSIT
+    # Contact Information:       12345678900
+    # Address:                   123 st. ABC village...
     pass
 
 
@@ -18,16 +35,17 @@ def update_student():
     # TODO (Althea Aragon):
     # Add a function to update an existing student record.
     # - First, show the list of student records
-    # - Allow the user to input the index
-    # - Let the user update name and/or program
+    # - Allow the user to input the key
+    # - Let the user update the value of the inputted key
+    #   - coordinate with (Grace Lim) for restrictions
     pass
 
 
 def delete_student():
     # TODO (Hoshea Lopez):
     # Add a function to delete a student record.
-    # - Show the list of student records
-    # - Allow the user to input the index
+    # - Show a list of student names
+    # - Allow the user to input the name for the record they want to delete
     # - Confirm before deletion
     pass
 
